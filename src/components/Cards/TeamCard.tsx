8a0d0cc2db13c0d23e8d319cdb43899774c3c219
Karlee Milbury <karleemilbury@gmail.com>
2025-05-28 14:34:53 -0300

    Fixed cards and removed unused fields

--- src/components/Cards/TeamCard.tsx
+++ src/components/Cards/TeamCard.tsx
@@ -11,8 +11,12 @@ export const TeamCard = ({ image, name, position }: TeamCardProps) => {
   return (
     <div className="border border-aqua w-full p-8 font-body flex flex-col justify-between bg-navy-blue text-black rounded-[1.25rem] glow-blur overflow-hidden">
       {image && (
-        <div className="relative -mx-8 -mt-8 mb-4 overflow-hidden ">
-          <ResponsiveImage image={image} className="w-full object-cover " />
+        <div className="relative -mx-8 -mt-8 mb-4 overflow-hidden aspect-square">
+          <ResponsiveImage
+            image={image}
+            className="w-full object-cover aspect-square rounded-none"
+            imageHeightClassName="h-full "
+          />
         </div>
       )}
 
