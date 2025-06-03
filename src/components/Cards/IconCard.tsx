c841c1e2f27bb382223a42bcd22feda10b055de0
Karlee Milbury <karleemilbury@gmail.com>
2025-06-03 14:51:17 -0300

    fixed image displaying when empty

--- src/components/Cards/IconCard.tsx
+++ src/components/Cards/IconCard.tsx
@@ -1,6 +1,11 @@
 import React, { ReactNode } from "react";
 import { ResponsiveImage } from "../ResponsiveImage";
-import { ImageField, KeyTextField, RichTextField } from "@prismicio/client";
+import {
+  ImageField,
+  isFilled,
+  KeyTextField,
+  RichTextField,
+} from "@prismicio/client";
 import { PrismicRichText } from "@prismicio/react";
 
 interface IconCardProps {
@@ -22,11 +27,11 @@ export const IconCard: React.FC<IconCardProps> = ({
       className="w-full h-full font-body card-bg-gradient glow-blur border border-aqua text-black text-center px-6 py-12 md:px-8 md:py-16 flex flex-col justify-center items-center rounded-[1.25rem] overflow-hidden"
       {...props}
     >
-      {image && (
-        <div className="w-[125px] h-[125px] mb-6 ">
+      {isFilled.image(image) && (
+        <div className="w-[125px] h-[125px] mb-6">
           <ResponsiveImage
             image={image}
-            className="w-full h-full object-contain "
+            className="w-full h-full object-contain"
             imageHeightClassName=""
           />
         </div>
