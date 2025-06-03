89faf5667b0124f7d4f839a76dc878c0c318e24f
Karlee Milbury <karleemilbury@gmail.com>
2025-06-03 12:44:21 -0300

    Added gradient border

--- src/components/Cards/TeamCard.tsx
+++ src/components/Cards/TeamCard.tsx
@@ -9,24 +9,26 @@ interface TeamCardProps {
 
 export const TeamCard = ({ image, name, position }: TeamCardProps) => {
   return (
-    <div className="border border-aqua w-full p-8 font-body flex flex-col justify-between bg-navy-blue text-black rounded-[1.25rem] glow-blur overflow-hidden">
-      {image && (
-        <div className="relative -mx-8 -mt-8 mb-4 overflow-hidden aspect-square">
-          <ResponsiveImage
-            image={image}
-            className="w-full object-cover aspect-square rounded-none"
-            imageHeightClassName="h-full "
-          />
-        </div>
-      )}
-
-      <div className="flex-1 mt-2">
-        {name && (
-          <p className="font-extraBold ml-1 gradient-text inline-block text-label">
-            {name}
-          </p>
+    <div className="p-[1px] rounded-[1.25rem] gradient-border">
+      <div className="border border-transparent w-full p-8 font-body flex flex-col justify-between bg-navy-blue text-black rounded-[1.25rem] glow-blur overflow-hidden">
+        {image && (
+          <div className="relative -mx-8 -mt-8 mb-4 overflow-hidden aspect-square">
+            <ResponsiveImage
+              image={image}
+              className="w-full object-cover aspect-square rounded-none"
+              imageHeightClassName="h-full"
+            />
+          </div>
         )}
-        {position && <p className="text-base text-white ml-1">{position}</p>}
+
+        <div className="flex-1 mt-2">
+          {name && (
+            <p className="font-extraBold ml-1 gradient-text inline-block text-label">
+              {name}
+            </p>
+          )}
+          {position && <p className="text-base text-white ml-1">{position}</p>}
+        </div>
       </div>
     </div>
   );
