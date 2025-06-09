8b6f6e8d012de8e9b11ff3a73c51eaa1f0431d30
Karlee Milbury <karleemilbury@gmail.com>
2025-06-09 13:02:26 -0300

    Added multilink column

--- src/slices/MultiLinkColumn/index.tsx
+++ src/slices/MultiLinkColumn/index.tsx
@@ -1,51 +1,84 @@
-import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+"use client";
 
-/**
- * Props for `MultiLinkColumn`.
- */
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { FaPlus, FaMinus } from "react-icons/fa6";
+import { Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import React, { JSX } from "react";
+
 export type MultiLinkColumnProps =
   SliceComponentProps<Content.MultiLinkColumnSlice>;
 
-/**
- * Component for "MultiLinkColumn" Slices.
- */
-const MultiLinkColumn: FC<MultiLinkColumnProps> = ({ slice }) => {
+const MultiLinkColumn = ({ slice }: MultiLinkColumnProps): JSX.Element => {
+  const footerComponentStyling = {
+    heading4: ({ children }: { children: React.ReactNode }) => (
+      <h4 className="text-[1.625rem] font-[600]">{children}</h4>
+    ),
+  };
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="flex flex-col gap-5 bg-midnight"
     >
-      Placeholder component for multi_link_column (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
+      <div className="hidden md:flex flex-col gap-5">
+        <span>
+          <PrismicRichText
+            field={slice.primary.title}
+            components={footerComponentStyling}
+          />
+        </span>
+        {slice.variation === "default" && (
+          <div className="flex flex-col gap-5">
+            {slice.primary.link.map((item, index) => (
+              <PrismicNextLink
+                field={item}
+                key={index}
+                className="no-underline"
+              />
+            ))}
+          </div>
+        )}
+      </div>
+
+      <Disclosure as="div" className="md:hidden">
+        {({ open }) => (
+          <>
+            <DisclosureButton
+              className={`flex items-center justify-center gap-4 pl-2 w-full menu-link ${open ? "mb-3" : ""}`}
+            >
+              <PrismicRichText
+                field={slice.primary.title}
+                components={footerComponentStyling}
+              />
+              <div>
+                <FaPlus
+                  className={`h-4 w-4 font-bold ${open ? "hidden" : ""}`}
+                />
+                <FaMinus
+                  className={`h-4 w-4 font-bold ${open ? "" : "hidden"}`}
+                />
+              </div>
+            </DisclosureButton>
+            <DisclosurePanel className="pl-2 flex flex-col gap-5 mb-3 transition duration-200 ease-out text-center">
+              {slice.variation === "default" &&
+                slice.primary.link.map((item, index) => (
+                  <PrismicNextLink
+                    field={item}
+                    key={index}
+                    className="no-underline text-base"
+                  />
+                ))}
+            </DisclosurePanel>
+          </>
+        )}
+      </Disclosure>
     </section>
   );
 };
