f14fe65f38d3f9cccaa1e62a36e8745d2c88d38a
Karlee Milbury <karleemilbury@gmail.com>
2025-12-12 12:23:08 -0400

    style: removed testing color bg

--- src/slices/NumberedList/index.tsx
+++ src/slices/NumberedList/index.tsx
@@ -14,20 +14,18 @@ export type NumberedListProps = SliceComponentProps<Content.NumberedListSlice>;
  */
 const NumberedList: FC<NumberedListProps> = ({ slice }) => {
   return (
-    <div className="bg-midnight">
-      <Section
-        data-slice-type={slice.slice_type}
-        data-slice-variation={slice.variation}
-      >
-        <Container>
-          <NumberList
-            title={slice.primary.title}
-            body={slice.primary.body}
-            listItems={slice.primary.list}
-          />
-        </Container>
-      </Section>
-    </div>
+    <Section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <Container>
+        <NumberList
+          title={slice.primary.title}
+          body={slice.primary.body}
+          listItems={slice.primary.list}
+        />
+      </Container>
+    </Section>
   );
 };
 
