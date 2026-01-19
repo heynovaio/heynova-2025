1f6796e6808b7beb56187cb05816a36da4b83ba5
Karlee Milbury <karleemilbury@gmail.com>
2026-01-19 15:05:55 -0400

    fix: only display language switcher when there is more than one language

--- src/components/Menu/Announcement.tsx
+++ src/components/Menu/Announcement.tsx
@@ -22,7 +22,7 @@ export const Announcement: React.FC<AnnouncementProps> = ({
         }}
       />
 
-      {locales && (
+      {locales && locales.length > 1 && (
         <div className="absolute top-0 right-4 z-[60] ">
           <LanguageSwitcher locales={locales} classname="hidden lg:block" />
         </div>
