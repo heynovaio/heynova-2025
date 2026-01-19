533aed4c091d54f5fad48ba527735afc0e2db91a
Karlee Milbury <karleemilbury@gmail.com>
2026-01-19 15:09:35 -0400

    fix: set language switcher on mobile to only display when more than one language

--- src/components/Menu/Header.tsx
+++ src/components/Menu/Header.tsx
@@ -79,7 +79,11 @@ export const Header: React.FC<HeaderProps> = ({
             prefetch={true}
             href={`/`}
           >
-            <PrismicNextImage field={logo} fallbackAlt="" className="md:px-5 py-5 px-0" />
+            <PrismicNextImage
+              field={logo}
+              fallbackAlt=""
+              className="md:px-5 py-5 px-0"
+            />
           </PrismicNextLink>
 
           {/* Desktop Menu - flex-1 to take space */}
@@ -92,59 +96,61 @@ export const Header: React.FC<HeaderProps> = ({
             <CalendlyButton text="Book a Chat" buttonClass="btn-primary" />
           </div>
 
-        {/* Mobile Menu */}
-        <div className="lg:hidden">
-          <Popover className="relative">
-            {({ open }) => (
-              <>
-                <PopoverButton
-                  className="inline-flex justify-center w-full p-2 relative group"
-                  aria-label="Main Menu"
-                >
-                  <div className="flex flex-col justify-center items-center w-8 h-8">
-                    <span
-                      className={`block absolute h-0.5 w-8 gradient-light-full transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-0" : "-translate-y-2"}`}
-                    ></span>
-                    <span
-                      className={`block absolute h-0.5 w-8 gradient-light-full transform transition duration-300 ease-in-out ${open ? "opacity-0" : "opacity-100"}`}
-                    ></span>
-                    <span
-                      className={`block absolute h-0.5 w-8 gradient-light-full transform transition duration-300 ease-in-out ${open ? "-rotate-45 translate-y-0" : "translate-y-2"}`}
-                    ></span>
-                  </div>
-                </PopoverButton>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="transform opacity-0 translate-x-full"
-                  enterTo="transform opacity-100 translate-x-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="transform opacity-100 translate-x-0"
-                  leaveTo="transform opacity-0 translate-x-full"
-                >
-                  <PopoverPanel
-                    anchor="bottom"
-                    className="w-screen h-screen bg-midnight bg-gradient-dark lg:mt-4 pb-10 z-40"
+          {/* Mobile Menu */}
+          <div className="lg:hidden">
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <PopoverButton
+                    className="inline-flex justify-center w-full p-2 relative group"
+                    aria-label="Main Menu"
                   >
-                    <div className="flex flex-col px-6 my-10 items-center justify-center w-full">
-                      <SliceZone slices={slices} components={components} />
-                      <CalendlyButton
-                        text="Book a Chat"
-                        buttonClass="btn-primary"
-                      />
+                    <div className="flex flex-col justify-center items-center w-8 h-8">
+                      <span
+                        className={`block absolute h-0.5 w-8 gradient-light-full transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-0" : "-translate-y-2"}`}
+                      ></span>
+                      <span
+                        className={`block absolute h-0.5 w-8 gradient-light-full transform transition duration-300 ease-in-out ${open ? "opacity-0" : "opacity-100"}`}
+                      ></span>
+                      <span
+                        className={`block absolute h-0.5 w-8 gradient-light-full transform transition duration-300 ease-in-out ${open ? "-rotate-45 translate-y-0" : "translate-y-2"}`}
+                      ></span>
                     </div>
-                    <LanguageSwitcher
-                      locales={locales}
-                      classname="bottom-0 fixed right-0"
-                    />
-                  </PopoverPanel>
-                </Transition>
-              </>
-            )}
-          </Popover>
-        </div>
-      </nav>
-    </header>
+                  </PopoverButton>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="transform opacity-0 translate-x-full"
+                    enterTo="transform opacity-100 translate-x-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="transform opacity-100 translate-x-0"
+                    leaveTo="transform opacity-0 translate-x-full"
+                  >
+                    <PopoverPanel
+                      anchor="bottom"
+                      className="w-screen h-screen bg-midnight bg-gradient-dark lg:mt-4 pb-10 z-40"
+                    >
+                      <div className="flex flex-col px-6 my-10 items-center justify-center w-full">
+                        <SliceZone slices={slices} components={components} />
+                        <CalendlyButton
+                          text="Book a Chat"
+                          buttonClass="btn-primary"
+                        />
+                      </div>
+                      {locales && locales.length > 1 && (
+                        <LanguageSwitcher
+                          locales={locales}
+                          classname="bottom-0 fixed right-0"
+                        />
+                      )}
+                    </PopoverPanel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
