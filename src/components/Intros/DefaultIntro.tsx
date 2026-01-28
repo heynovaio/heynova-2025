1d64843b5082169b57f75883ad8711226c65011a
Karlee Milbury <karleemilbury@gmail.com>
2026-01-28 12:39:27 -0400

    hotfix: fixed title cutting off on default intro

--- src/components/Intros/DefaultIntro.tsx
+++ src/components/Intros/DefaultIntro.tsx
@@ -40,7 +40,7 @@ export const DefaultIntro: React.FC<GeneralHeroProps> = ({ data }) => {
               field={data.title}
               components={{
                 heading1: ({ children }) => (
-                  <h1 className=" gradient-text">{children}</h1>
+                  <h1 className=" gradient-text pb-2">{children}</h1>
                 ),
               }}
             />
