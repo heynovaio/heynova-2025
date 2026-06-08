0fdf674eb97c92580eae44fe8e9f8187a0324449
Karlee Milbury <karleemilbury@gmail.com>
2026-06-08 13:03:00 -0300

    feat: added services grid slice

--- src/slices/ServicesGrid/index.tsx
+++ src/slices/ServicesGrid/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Content, isFilled, asText } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { Container, Section } from "@/components/Layout";
 import { Grid } from "@/components/Grid/Grid";
 import { GeneralCard } from "@/components/Cards";
@@ -8,12 +8,14 @@ import { GeneralCard } from "@/components/Cards";
 export type ServicesGridProps = SliceComponentProps<Content.ServicesGridSlice>;
 
 const ServicesGrid: FC<ServicesGridProps> = ({ slice }) => {
+  console.log("ServicesGrid slice data:", slice);
   return (
     <Section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
       <Container>
+        <PrismicRichText field={slice.primary.title} />
         <div className="my-8 md:mb-16">
           <Grid maxColumns={3}>
             {(slice.primary.services || []).map((item, index) => {
@@ -33,10 +35,10 @@ const ServicesGrid: FC<ServicesGridProps> = ({ slice }) => {
                 <GeneralCard
                   key={index}
                   href={service.url || "#"}
-                  // image={data.meta_image}
                   title={data.meta_title || asText(data.title) || "Untitled"}
                   titleLevel={3}
                   description={data.meta_description || data.body}
+                  titleClassName={"text-aqua"}
                 />
               );
             })}
