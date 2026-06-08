7755d481b0f6b4bf7c62f304729d48b75e2293db
Karlee Milbury <karleemilbury@gmail.com>
2026-06-08 12:03:55 -0300

    prismic: added servicesgrid slice to service listing page

--- src/slices/ServicesGrid/index.tsx
+++ src/slices/ServicesGrid/index.tsx
@@ -1,30 +1,49 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
+import { Content, isFilled, asText } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { Container, Section } from "@/components/Layout";
+import { Grid } from "@/components/Grid/Grid";
+import { GeneralCard } from "@/components/Cards";
 
-/**
- * Props for `ServicesGrid`.
- */
 export type ServicesGridProps = SliceComponentProps<Content.ServicesGridSlice>;
 
-/**
- * Component for "ServicesGrid" Slices.
- */
 const ServicesGrid: FC<ServicesGridProps> = ({ slice }) => {
   return (
-    <section
+    <Section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for services_grid (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use your own AI agent with the Prismic CLI
-       * 📚 Docs: https://prismic.io/docs/ai#create-slices
-       */}
-    </section>
+      <Container>
+        <div className="my-8 md:mb-16">
+          <Grid maxColumns={3}>
+            {(slice.primary.services || []).map((item, index) => {
+              const service = item.service;
+              if (!isFilled.contentRelationship(service) || !service.data)
+                return null;
+
+              const data = service.data as {
+                title?: any;
+                body?: any;
+                meta_title?: string;
+                meta_description?: string;
+                meta_image?: any;
+              };
+
+              return (
+                <GeneralCard
+                  key={index}
+                  href={service.url || "#"}
+                  // image={data.meta_image}
+                  title={data.meta_title || asText(data.title) || "Untitled"}
+                  titleLevel={3}
+                  description={data.meta_description || data.body}
+                />
+              );
+            })}
+          </Grid>
+        </div>
+      </Container>
+    </Section>
   );
 };
 
