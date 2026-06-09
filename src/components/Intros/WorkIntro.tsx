dcee4bf1b693311aba461ef8d063fff334aa41bc
Karlee Milbury <karleemilbury@gmail.com>
2026-06-09 09:23:03 -0300

    feat: added new intro to work page

---
+++ src/components/Intros/WorkIntro.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React from "react";
+import { Container } from "../Layout";
+import { PrismicRichText } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
+import { ContentRelationshipField } from "@prismicio/client";
+import Link from "next/link";
+import { Simplify, WorkExampleDocumentData } from "../../../prismicio-types";
+
+interface WorkIntroProps {
+  data: Simplify<WorkExampleDocumentData>;
+  id?: string | ContentRelationshipField;
+  content?: React.ReactNode;
+  lang?: string;
+  uid?: string;
+}
+
+export const WorkIntro: React.FC<WorkIntroProps> = ({ data, uid }) => {
+  const imageExists = data.image?.url;
+
+  return (
+    <section
+      className={`${
+        imageExists ? "min-h-[600px] py-16" : "py-16  md:h-auto"
+      }  flex items-center hero-content print:py-6 `}
+    >
+      <Container>
+        <div
+          className={`text-white flex flex-col sm:flex-row justify-between ${
+            imageExists ? "sm:items-center" : "items-center"
+          } padded-div py-0 pb-12 gap-8 mt-0 gradient-bottom-border`}
+        >
+          <div
+            className={`${
+              imageExists ? "basis-1/2" : "text-center grow mx-auto"
+            } hero-container flex flex-col gap-8  order-1 max-w-[750px] sm:order-0 print-text`}
+          >
+            <span>
+              <Link href={"/"}>Home</Link> /{" "}
+              <Link href={"/our-work"}>Work Examples</Link>
+              <Link href={`/${uid || ""}`}></Link>
+            </span>
+            <PrismicRichText
+              field={data.title}
+              components={{
+                heading1: ({ children }) => (
+                  <h1 className="gradient-text">{children}</h1>
+                ),
+              }}
+            />
+            {/* {data.body && <PrismicRichText field={data.body} />} */}
+          </div>
+
+          {imageExists && (
+            <div className="basis-1/2 order-0 sm:order-1 print:mt-6 print:mb-0">
+              <PrismicNextImage
+                field={data.image}
+                className="w-auto object-cover object-center rounded"
+                fallbackAlt=""
+                priority
+                sizes="(min-width: 640px) 50vw, 100vw"
+              />
+            </div>
+          )}
+        </div>
+      </Container>
+    </section>
+  );
+};
